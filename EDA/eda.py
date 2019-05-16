4f8bea23374270d30a3cdca7f963f42512e07d7c
senkin13 <senkin13@gmail.com>
2019-05-16 19:08:43 +0900

    Update and rename scatter.py to eda.py

--- EDA/eda.py
+++ EDA/eda.py
@@ -5,8 +5,16 @@ color = sns.color_palette()
 
 target_col = "target"
 
+##### scatter
 plt.figure(figsize=(8,6))
 plt.scatter(range(train.shape[0]), np.sort(train[target_col].values))
 plt.xlabel('index', fontsize=12)
 plt.ylabel('Target', fontsize=12)
+plt.show()
+
+##### Histogram
+plt.figure(figsize=(12,8))
+sns.distplot(train[target_col].values, bins=50, kde=False, color="red")
+plt.title("Histogram of Target")
+plt.xlabel('Target', fontsize=12)
 plt.show()
