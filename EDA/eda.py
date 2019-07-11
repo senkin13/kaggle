58e39725f20277c7f6a0da919ed4cd2eddc3241d
senkin13 <senkin13@gmail.com>
2019-07-11 13:30:06 +0900

    Update eda.py

--- EDA/eda.py
+++ EDA/eda.py
@@ -1,3 +1,5 @@
+from sklearn.metrics import confusion_matrix
+import itertools
 import matplotlib.pyplot as plt
 import seaborn as sns
 color = sns.color_palette()
@@ -28,3 +30,46 @@ plt.title('Testing Set')
 plt.xlabel('time to failure', fontsize=12)
 plt.ylabel('predicted', fontsize=12)
 plt.plot([(0, 0), (20, 20)], [(0, 0), (20, 20)])
+
+
+##### confusion_matrix
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+    
+cnf_matrix = confusion_matrix(train_df['outliers'], (oof_preds>0.5))# np.round
+
+plt.figure(figsize=(10,10))
+plot_confusion_matrix(cnf_matrix, classes=[0,1],
+                      title='Confusion matrix')
